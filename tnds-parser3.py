653a9f790f85220278faaf3a3de1d0d2f60a1811
Jon Warbrick <jw35@cam.ac.uk>
2018-03-10 15:10:32 +0000

    Fix pycodestyle lint warb=nings

--- tnds-parser3.py
+++ tnds-parser3.py
@@ -13,9 +13,11 @@ from datetime import datetime
 ns = {'n': 'http://www.transxchange.org.uk/'}
 
 
-def display_stop(tree,ref):
+def display_stop(tree, ref):
     """ Given a StopPointRef of an AnnotatedStopPoint, return a description """
-    stop = tree.find("n:StopPoints/n:AnnotatedStopPointRef[n:StopPointRef='%s']" % ref, ns)
+    stop = tree.find(
+      "n:StopPoints/n:AnnotatedStopPointRef[n:StopPointRef='%s']" % ref, ns
+    )
     result = r'%s\n' % ref
     if stop.find('n:Indicator', ns) is not None:
         result = "%s%s" % (result, stop.find('n:Indicator', ns).text)
@@ -46,85 +48,112 @@ def process(filename, index):
     # Counting on there being only one Service, Line and Operator
     # in each file...
 
-    service_description = tree.find('n:Services/n:Service/n:Description', ns).text
-    operator_name = tree.find('n:Operators/n:Operator/n:OperatorShortName', ns).text
-    line_name = tree.find('n:Services/n:Service/n:Lines/n:Line/n:LineName', ns).text
+    service_description = tree.find(
+      'n:Services/n:Service/n:Description', ns
+    ).text
+    operator_name = tree.find(
+      'n:Operators/n:Operator/n:OperatorShortName', ns
+    ).text
+    line_name = tree.find(
+      'n:Services/n:Service/n:Lines/n:Line/n:LineName', ns
+    ).text
 
-    print("<h2>%s - %s (%s)</h2>" % (line_name, service_description, basename), file=index)
+    print("<h2>%s - %s (%s)</h2>" %
+          (line_name, service_description, basename), file=index)
     print("<ul>", file=index)
 
     # For each route...
     for route in tree.findall('n:Routes/n:Route', ns):
-      route_id = route.get('id')
-      private_code = route.find('n:PrivateCode', ns).text
-      route_section_ref = route.find('n:RouteSectionRef', ns).text
+        route_id = route.get('id')
+        route_section_ref = route.find('n:RouteSectionRef', ns).text
 
-      # ...for each RouteLink in that RouteSection for that Route
-      route_section = tree.find("n:RouteSections/n:RouteSection[@id='%s']" % route_section_ref, ns)
-      first_link_id = route_section.find("n:RouteLink[1]" , ns).get('id')
-      last_link_id = route_section.find("n:RouteLink[last()]" , ns).get('id')
-      for link in route_section.findall('n:RouteLink', ns):
+        # ...for each RouteLink in that RouteSection for that Route
+        route_section = tree.find(
+          "n:RouteSections/n:RouteSection[@id='%s']" % route_section_ref, ns
+        )
+        first_link_id = route_section.find("n:RouteLink[1]", ns).get('id')
+        last_link_id = route_section.find("n:RouteLink[last()]", ns).get('id')
+        for link in route_section.findall('n:RouteLink', ns):
 
-        from_stop = link.find('n:From/n:StopPointRef', ns).text
-        to_stop = link.find('n:To/n:StopPointRef', ns).text
-        direction = link.find('n:Direction', ns).text
+            from_stop = link.find('n:From/n:StopPointRef', ns).text
+            to_stop = link.find('n:To/n:StopPointRef', ns).text
+            direction = link.find('n:Direction', ns).text
 
-        # Make a new graph if we haven't seen this direction before
-        # (1 graph per direction else it just gets too confusing)
-        if direction not in graphs:
-          graphs[direction] = Digraph(graph_attr=graph_attrs,
-            node_attr=node_attrs,
-            edge_attr=edge_attrs,
-            name='%s %s %s' % (line_name, direction, service_description))
-          graphs[direction].attr(label=r'\n%s %s %s\n%s\n%s' % (operator_name, line_name, direction, service_description, basename))
-          graphs[direction].attr(fontsize='20')
+            # Make a new graph if we haven't seen this direction before
+            # (1 graph per direction else it just gets too confusing)
+            if direction not in graphs:
+                graphs[direction] = Digraph(graph_attr=graph_attrs,
+                                            node_attr=node_attrs,
+                                            edge_attr=edge_attrs,
+                                            name='%s %s %s' %
+                                            (line_name, direction,
+                                             service_description))
+                graphs[direction].attr(label=r'\n%s %s %s\n%s\n%s' %
+                                       (operator_name, line_name, direction,
+                                        service_description, basename))
+                graphs[direction].attr(fontsize='20')
 
-        # Form a sequence number for this route in this direction
-        if direction not in route_serial:
-          route_serial[direction] = {}
-        if route_id not in route_serial[direction]:
-          if direction not in serial_counter:
-            serial_counter[direction] = 0
-          else:
-            serial_counter[direction] += 1
-          route_serial[direction][route_id] = serial_counter[direction]
-        route_no = route_serial[direction][route_id]
+            # Form a sequence number for this route in this direction
+            if direction not in route_serial:
+                route_serial[direction] = {}
+            if route_id not in route_serial[direction]:
+                if direction not in serial_counter:
+                    serial_counter[direction] = 0
+                else:
+                    serial_counter[direction] += 1
+                route_serial[direction][route_id] = serial_counter[direction]
+            route_no = route_serial[direction][route_id]
 
-        route_color = colors[route_no % len(colors)]
+            route_color = colors[route_no % len(colors)]
 
-        # Label first and last nodes and links differently
-        label = str(route_no + 1)
+            # Label first and last nodes and links differently
+            label = str(route_no + 1)
 
-        if link.get('id') == first_link_id:
-          label = '<<font face="helvetica-bold">%d (first)</font>>' % (route_no + 1)
-        elif link.get('id') == last_link_id:
-          label = '<<font face="helvetica-bold">%d (last)</font>>' % (route_no + 1)
+            if link.get('id') == first_link_id:
+                label = ('<<font face="helvetica-bold">%d (first)</font>>' %
+                         (route_no + 1))
+            elif link.get('id') == last_link_id:
+                label = ('<<font face="helvetica-bold">%d (last)</font>>' %
+                         (route_no + 1))
 
-        if link.get('id') == first_link_id:
-          graphs[direction].node(from_stop, display_stop(tree,from_stop), penwidth="4")
-        else:
-          graphs[direction].node(from_stop, display_stop(tree,from_stop))
-        if link.get('id') == last_link_id:
-          graphs[direction].node(to_stop, display_stop(tree,to_stop), penwidth="4")
-        else:
-          graphs[direction].node(to_stop, display_stop(tree,to_stop))
-        graphs[direction].edge(from_stop, to_stop, color=route_color, label=label)
+            if link.get('id') == first_link_id:
+                graphs[direction].node(from_stop,
+                                       display_stop(tree, from_stop),
+                                       penwidth="4")
+            else:
+                graphs[direction].node(from_stop,
+                                       display_stop(tree, from_stop))
+            if link.get('id') == last_link_id:
+                graphs[direction].node(to_stop,
+                                       display_stop(tree, to_stop),
+                                       penwidth="4")
+            else:
+                graphs[direction].node(to_stop,
+                                       display_stop(tree, to_stop))
+            graphs[direction].edge(from_stop,
+                                   to_stop,
+                                   color=route_color,
+                                   label=label)
 
     # Draw each graph (i.e. for each identified direction)
     for direction in sorted(graphs):
-      #text = '<<table>'
-      #for id, counter in route_serial[direction].items():
-      #  text = '%s<tr><td>%s</td><td>%s</td></tr>' % (text, counter, id)
-      #text = text + '</table>>'
-      #graphs[direction].node('key', text)
-      print(graphs[direction].render('%s-%s' % (basename,direction), cleanup=True))
-      print("<li><a href=%s-%s.pdf>%s</a></li>" % (basename, direction, direction), file=index)
+        # text = '<<table>'
+        # for id, counter in route_serial[direction].items():
+        #   text = '%s<tr><td>%s</td><td>%s</td></tr>' % (text, counter, id)
+        # text = text + '</table>>'
+        # graphs[direction].node('key', text)
+        print(graphs[direction].render(
+          '%s-%s' % (basename, direction), cleanup=True
+        ))
+        print("<li><a href=%s-%s.pdf>%s</a></li>" %
+              (basename, direction, direction), file=index)
 
     print("</ul>", file=index)
 
+
 if __name__ == '__main__':
 
-    index = open('index.html', 'w');
+    index = open('index.html', 'w')
 
     print('''
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
